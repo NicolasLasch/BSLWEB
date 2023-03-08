67b4252aeb9394e046a243a5aa0b7f2686d2172c
NicolasLasch <Nicolas.Lasch@gmail.com>
2023-03-08 23:33:21 +0100

    eds

--- setup.py
+++ setup.py
@@ -1,23 +1,12 @@
-import setuptools
+from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="bsl",
-    version="0.0.1",
-    author="Nicolas LAsch",
-    author_email="",
-    description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/NicolasLasch/BSLWEB",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-    install_requires=[line.strip() for line in open('requirements.txt')],
+setup(
+    name='bls',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'requests>=2.25.1',
+        'numpy>=1.20.1',
+        'pandas>=1.2.2'
+    ]
 )
